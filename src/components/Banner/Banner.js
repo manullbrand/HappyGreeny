5014870c18bc2a3970d703bdba64c4b2134724f7
Manuela Brandao <manulbrandao@gmail.com>
2023-07-20 17:43:53 +0200

    Adjusted Banner and Categories to match a responsive layout

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,20 +1,27 @@
 import "./Banner.css";
-import image from "../../assets/images/dieffenbachia_banner.jpeg";
+import image1 from "../../assets/images/dieffenbachia_banner.jpeg";
+import image2 from "../../assets/images/table_plants.jpeg";
 import leafIcon from "../../assets/images/leaf_icon3.svg";
 
 export default function Banner() {
   return (
     <header className="App-header">
-      <h1>
-        SmilyGreeny
-        <img className="App-header__logoTitle" src={leafIcon} />
-      </h1>
-
-      <h2>Organize your indoor plants!</h2>
+      <div className="App-header__bannerText">
+        <h1 className="App-header__h1">
+          SmilyGreeny
+          <img className="App-header__logoTitle" src={leafIcon} />
+        </h1>
+        <h2 className="App-header__h2">Organize your indoor plants!</h2>
+      </div>
+      <img
+        className="App-header__imgBanner2"
+        src={image2}
+        alt="Bamboo plant on a vase "
+      />
       <img
         className="App-header__imgBanner"
-        src={image}
-        alt="Areca plant in a living room"
+        src={image1}
+        alt="Plant in a living room"
       />
     </header>
   );
